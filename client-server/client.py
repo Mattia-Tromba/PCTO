2344eb881fffe62745e671d1dd1a76b49107a20a
Mattia-Tromba <mattiatromba07@gmail.com>
2025-09-18 17:37:43 +0200

    modifica incompleta: server che registra/autentica i client e aggiunta database

--- client-server/client.py
+++ client-server/client.py
@@ -5,10 +5,22 @@ def client_program():
     client.connect(('localhost', 5000))
     print(client)
     message = ""
-    while message != "bye":
+    while message != "r" or message != "a":
         message = input(" -> ")
         client.send(message.encode())
         print(client.recv(1024).decode())
+    if message == "r":
+        print(client.recv(1024).decode())
+        client.send(input(" -> ").encode())
+        print(client.recv(1024).decode())
+    else:
+        while True:
+            print(client.recv(1024).decode())
+            client.send(input(" -> ").encode())
+            if client.recv(1024).decode() == "invia il token: ":
+                client.send(input(" -> ").encode())
+                print(client.recv(1024).decode())
+                break
     client.close()
 
 client_program()
--- client-server/server.py
+++ client-server/server.py
@@ -1,4 +1,4 @@
-import socket, threading
+import socket, threading, sqlite3, secrets, string
 
 host = "localhost"
 port = 5000
@@ -8,13 +8,46 @@ print("Starting...")
 server_socket.listen(6)
 
 def server_program(conn, address):
-    while True:
+    connessione = sqlite3.connect(r'C:\Users\NADIA\workspace\CPTO\database\database.db')
+    c = connessione.cursor()
+    c.execute("create table if not exists users (email varchar[50], token varchar[27])")
+    carattere = ""
+
+    while carattere != 'r' and carattere != 'a':
         data = conn.recv(1024).decode()
         if not data:
             break
         print("Server received: ", data, "from port: ", address)
-        data = input("Respond to %d: " % address[1])
-        conn.send(data.encode())
+        conn.send("Vuoi registrarti o autenticarti? (r/a)".encode("utf-8"))
+        carattere = conn.recv(1024).decode()
+
+        if carattere != 'r' and carattere != 'a':
+            conn.send("inserisci o la lettera a o la lettera r".encode("utf-8"))
+
+    if carattere == "r":
+        conn.send("invia la tua email per la registrazione: ".encode("utf-8"))
+        email = conn.recv(1024).decode()
+        caratteri = string.ascii_letters + string.digits
+        token = ''.join(secrets.choice(caratteri) for i in range(26))
+        conn.send(("questo è il tuo token: %s" %token).encode("utf-8"))
+        c.execute("insert into users (email, token) values (?, ?)", (email, token))
+        connessione.commit()
+    else:
+        successo = True
+
+        while successo:
+                conn.send("invia la tua email per il controllo: ".encode("utf-8"))
+                email = conn.recv(1024).decode()
+                if c.execute("select email from users where email = ?", (email,)).fetchone():
+                    conn.send("invia il token: ".encode("utf-8"))
+                    token = conn.recv(1024).decode()
+                    if c.execute("select token from users where token = ?", (token,)).fetchone():
+                        conn.send("autenticazione effettuata".encode("utf-8"))
+                        successo = False
+                    else:
+                        conn.send("il token è errato".encode("utf-8"))
+                else:
+                    conn.send("l'email che hai inserito è sbagliata \n".encode("utf-8"))
     conn.close()
 
 while True:
