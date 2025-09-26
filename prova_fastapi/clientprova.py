c9481ff4e4fe59eb03ea616fab7eef8913fbcf9b
gCap-pixel <gcappellin9@gmail.com>
2025-09-26 11:01:58 +0200

    aggiunta di numero di utenti, possibilità di rimuovere iscrizione, iscrizioni da data x

--- prova_fastapi/clientprova.py
+++ prova_fastapi/clientprova.py
@@ -1,5 +1,10 @@
 import requests
 
-r = requests.get("http://127.0.0.1:8000/db/trova/9")
-print(r.text)
-#oj
+#r=requests.post("http://127.0.0.1:8000/db/create")
+#a = requests.get("http://127.0.0.1:8000/db/iscrizioni/dal/2025-09-28/ad/oggi")
+b = requests.get("http://127.0.0.1:8000/db/numero/utenti")
+c= requests.delete("http://127.0.0.1:8000/db/rimuovi/caparezza/iovengodallaluna")
+print(c.text)
+print(b.text)
+"print (r.text)"
+"print(a.text)"
--- prova_fastapi/main.py
+++ prova_fastapi/main.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-
+from datetime import date, datetime
 from fastapi import FastAPI, Depends, HTTPException
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
@@ -11,8 +11,10 @@ engine = create_engine(urldb)
 
 #modello tabella database
 class Users(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    email: str = Field(index=True)
+    id: int = Field(primary_key=True)
+    email: str = Field(unique=True)
+    token: str = Field(unique=True)
+    data: date = Field(default=None)
 
 def crea_tabella():
     SQLModel.metadata.create_all(engine)
@@ -44,11 +46,25 @@ def trova(id: int, session: SessionDep) -> Users:
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
-@app.delete("/db/rimuovi/{id}")
-def rimuovi(id: int, session: SessionDep):
-    user = session.get(Users, id)
-    if user is None:
+@app.delete("/db/rimuovi/{email}/{token}")
+def rimuovi(email: str, token : str, session: SessionDep):
+    persona = session.exec(select(Users).where(Users.email == email, Users.token == token)).first()
+    if persona is None:
         raise HTTPException(status_code=404, detail="User not found")
-    session.delete(user)
+    session.delete(persona)
     session.commit()
     return "user deleted"
+
+@app.get("/db/iscrizioni/dal/{data}/ad/oggi")
+def iscrizioni_da(data : date, session: SessionDep):
+    oggi = date.today()
+    richiesta = session.exec(select(Users).where((Users.data >= data) & (Users.data <= oggi))).all()
+    if richiesta is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return richiesta
+
+@app.get("/db/numero/utenti")
+def numero_utenti(session: SessionDep):
+    utenti = session.exec(select(Users)).all()
+    numero = len(utenti)
+    return numero
