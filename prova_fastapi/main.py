40f8d2339d137136b2a6cf18adbf55fe831a84dd
Mattia-Tromba <mattiatromba07@gmail.com>
2025-09-26 10:54:22 +0200

    aggiornamento server

--- prova_fastapi/main.py
+++ prova_fastapi/main.py
@@ -1,7 +1,9 @@
+import json
 from typing import Annotated
-
+from datetime import date
 from fastapi import FastAPI, Depends, HTTPException
 from sqlmodel import Field, Session, SQLModel, create_engine, select
+import secrets, re, string
 
 app = FastAPI()
 
@@ -12,8 +14,10 @@ engine = create_engine(urldb)
 
 #modello tabella database
 class Users(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    email: str = Field(index=True)
+    id: int = Field(primary_key=True)
+    email: str = Field(unique=True)
+    token: str = Field(unique=True)
+    data: date = Field(default=None)
 
 def crea_tabella():
     SQLModel.metadata.create_all(engine)
@@ -23,7 +27,6 @@ def get_session():
     with Session(engine) as session:
         yield session
 
-
 SessionDep = Annotated[Session, Depends(get_session)]
 
 @app.post("/db/create")
@@ -31,25 +34,45 @@ def create():
     crea_tabella()
     return {"table": "created"}
 
-@app.put("/db/aggiungi")
-def aggiungi(user: Users, session: SessionDep) -> Users:
-    session.add(user)
-    session.commit()
-    session.refresh(user)
-    return user
+@app.put("/db/registrazione")
+def registrazione(email: str, session: SessionDep):
+    if re.match(r'^\w+@\w+\.\w', email):
+        caratteri = string.ascii_letters + string.digits
+        tk = ''.join(secrets.choice(caratteri) for _ in range(26))
+        user = Users(email=email, token=tk, data=date.today())
+        session.add(user)
+        session.commit()
+        return {"registrazione completata": "token: %s" %tk}
+    raise HTTPException(status_code=400, detail="Email non valida")
 
-@app.get("/db/trova/{id}")
-def trova(id: int, session: SessionDep) -> Users:
-    user = session.get(Users, id)
+@app.get("/db/autenticazione")
+def autenticazione(email: str, token: str, session: SessionDep):
+    user = session.scalars(select(Users).where(Users.email == email)).first()
     if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+        raise HTTPException(status_code=404, detail="Email non valida")
+    if user.token != token:
+        raise HTTPException(status_code=404, detail="Token non valido")
+    return {"autenticazione" : "effettuata"}
 
-@app.delete("/db/rimuovi/{id}")
-def rimuovi(id: int, session: SessionDep):
-    user = session.get(Users, id)
+@app.get("/db/intervallo")
+def intervallo(id1, id2: int, session: SessionDep):
+    dict = {}
+    for x in range(int(id1), int(id2)+1):
+        user = session.get(Users, x)
+        if user is None:
+            raise HTTPException(status_code=404, detail="First user not found")
+        dict.update({f"user_{x}": user.email})
+    return dict
+
+@app.put("/db/cambiatoken")
+def cambiatk(email: str, token: str, session: SessionDep):
+    user = session.scalars(select(Users).where(Users.email == email)).first()
     if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    session.delete(user)
-    session.commit()
-    return "user deleted"
+        raise HTTPException(status_code=404, detail="user not found")
+    if user.token == token:
+        caratteri = string.ascii_letters + string.digits
+        tk = ''.join(secrets.choice(caratteri) for _ in range(26))
+        user.token = tk
+        session.commit()
+        return {"token": "aggiornato"}
+    raise HTTPException(status_code=404, detail="Token non valido")
